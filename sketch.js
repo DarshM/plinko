8426e1afe3cf6fb5e1395aad8fc5600ae33a0431
DarshM <71961669+DarshM@users.noreply.github.com>
2020-11-07 11:13:53 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,61 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var particles=[];
+var plinkos=[];
+var divisions=[];
+var divisionHeight=300;
+var ground;
+function setup() {
+  createCanvas(480,800);
+  //createSprite(400, 200, 50, 50);
+  ground=new Ground(780,240,1000,10);
+
+  engine=Engine.create();
+  world=engine.world;
+}
+
+function draw() {
+  background(0); 
+  
+  for(var k=0;k<=width;k=k+80){
+  divisions.push(new Divisions(k,height-divisionHeight/2,10,divisionHeight));
+  }
+
+  for(var j=40;j<=width;j=j+50)
+  {
+
+    plinkos.push(new Plinko(j,75));
+  }
+
+  
+  for(var j=15;j<=width;j=j+50)
+  {
+
+    plinkos.push(new Plinko(j,175));
+  }
+
+  for(var j=0;j,plinkos.length;j++){
+
+    plinkos[j].display();
+  }
+
+
+  for(var k=0;j,divisions.length;k++){
+
+    divisions[k].display();
+  }
+
+  for(var j=0;j,particles.length;j++){
+
+    particles[j].display();
+  }
+
+
+  drawSprites();
+
+
+}
